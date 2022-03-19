4475bae8d3bb5b9ca20e03cfaf5b6a6f2a07e802
raxadavid <raxadavid@yahoo.com>
2022-03-18 21:35:50 -0700

    Added Scroll to top, resized windows, etc

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
+
+//Get the button
+var mybutton = document.getElementById("topButton");
+
+// When the user scrolls down 30px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
@@ -41,11 +63,11 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
 (function ($) {
-    // Typed Initiate
-    if ($('.hero .hero-text h2').length == 1) {
-        var typed_strings = $('.hero .hero-text .typed-text').text();
-        var typed = new Typed('.hero .hero-text h2', {
+  if ($('.hero-text h3').length == 1) {
+        var typed_strings = $('.hero-text .typed-text').text();
+        var typed = new Typed('.hero-text h3', {
             strings: typed_strings.split(', '),
             typeSpeed: 100,
             backSpeed: 20,
