69fb243fc9a1ca6cf928081261892b1539438414
raxadavid <raxadavid@yahoo.com>
2022-03-21 20:28:25 -0700

    fixed images, added projects, added javascript background potential

---
+++ scripts/sketch.js
@@ -0,0 +1,149 @@
+let trailBackground = [];
+var canvas
+var mouse;
+let squareBackground = [];
+let str = ["Design","UX Design", "Visual Design","Interaction Design", "Student","Web Development"]
+let color = ["#39B54A", "#009444", "#006838"];
+let bg;
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function preload() {
+        bg =loadImage('backgroundJS.png');
+}
+
+function setup() {
+
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index','-1');
+  canvas.elt.style.position = "fixed"
+  //noCursor();
+  
+      for (i=0; i<50; i++){
+    trailBackground[i] = new Trail(mouseX,mouseY,random(1,8),random(color),random(0.05,1.5));
+  }
+  
+  for (i=0; i<10; i++) {
+    squareBackground[i] = new Square(random(windowWidth/2,windowWidth) , random(0,windowHeight), random(50,150), random(1,50) ,random(0.1,0.5));
+  }
+}
+
+function draw() {
+    background(240);
+    for (i=0; i<10; i++) {
+    squareBackground[i].body();
+    squareBackground[i].movement();
+  }
+  
+  //trail background,  overlays all items
+  for (i=0; i<30; i++) {
+    trailBackground[i].trailBody();
+    trailBackground[i].ease();
+  }
+  
+
+  
+  //Mouse pointer
+  //fill ("white");
+  //circle(mouseX, mouseY, 20);
+}
+
+class Trail {
+  constructor(x,y,d,c,s) {
+    this.x = x;
+    this.y = y;
+    this.d = d;
+    this.c = c;
+    this.s = s;
+  }
+  
+  trailBody(){
+  //fill(this.c);
+  square(this.x,this.y,this.d);
+  }
+  
+  ease() {
+  let easing = this.s;
+  let targetX = mouseX;
+  let dx = targetX - this.x;
+  let targetY = mouseY;
+  let dy = targetY - this.y;
+  this.x += (dx * easing);
+  this.y += (dy * easing);
+  }
+}
+
+
+class Square {
+  constructor(x,y,d,c,s) {
+    this.x = x;
+    this.y = y;
+    this.d = d;
+    this.c = c;
+    this.s = s;
+    this.reverseMove = random([true,false]);
+    this.reverseMoveY = random([true,false]);
+    this.colorFull = false;
+    this.cSpeed = random(0.01,1);
+  }
+  
+  
+  
+  body() {
+    noFill();
+    square(this.x,this.y,this.d);
+    stroke(100);
+  }
+  
+  movement() {
+    if (this.x<windowWidth && this.reverseMove==true) {
+      this.x+=this.s;
+    }
+    //hit side
+    else if (this.x>windowWidth && this.reverseMove==true){
+     this.reverseMove = false;
+    }
+    else if (this.reverseMove==false && this.x>windowWidth/2) {
+      this.x-=this.s;
+    }
+    //hit side
+    else if (this.x<windowHeight && this.reverseMove==false) {
+      this.reverseMove = true;
+    }
+
+    
+//Moving up and down
+    if (this.y<windowHeight && this.reverseMoveY==true) {
+      this.y+=this.s;
+    }
+    else if (this.y>windowHeight && this.reverseMoveY==true){
+     this.reverseMoveY = false;
+    }
+    else if (this.reverseMoveY==false && this.y>0) {
+      this.y-=this.s;
+    }
+    
+    else if (this.y<windowHeight && this.reverseMoveY==false) {
+      this.reverseMoveY = true;
+    }
+    
+  }
+  
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
